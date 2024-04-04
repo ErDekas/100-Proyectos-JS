127d0744ca3aeaf87c151fffd3f0029336fedf06
ErDekas <pabletor0505@gmail.com>
2024-04-04 19:42:28 +0200

    Avances del generador

--- 06-Generador de Contraseñas/index.js
+++ 06-Generador de Contraseñas/index.js
@@ -11,6 +11,7 @@ function copyToClipBoard(){
 let letras =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 let numeros = ['1','2','3','4','5','6','7','8','9','0'];
 let simbolos = ['~','`','!','@','#','$','%','^','&','*','(',')','-','_','+','=','{','[','}',']','|',':',';','"','<','>','?','/','\\'];
+let contrasena = [];
 /**
  * Generates a password based on the selected options.
  *
@@ -23,9 +24,30 @@ function generarContrasena(){
     let numeros = document.getElementById('numeros').checked;
     let longitud = document.getElementById('longitud').value;
 
-    if((letras || simbolos || numeros)==true){
-        contrasena.value = generarPassword(letras, simbolos, numeros, longitud);
-    }else{
-        alert('Debes seleccionar al menos una opción');
+    recorrerArrays(letras, simbolos, numeros);
+    }
+    
+console.log(letras.length);
+console.log(numeros.length);
+console.log(simbolos.length);
+let suma = letras.length+numeros.length+simbolos.length;
+console.log(suma);
+
+function recorrerArrays(letras, simbolos, numeros) {
+    if (letras == checked) {
+        letras.forEach(function (element, indice, _array) {
+            console.log(element, indice);
+        });
+
+    }
+    if (simbolos == checked) {
+        simbolos.forEach(function (element, indice, _array) {
+            console.log(element, indice);
+        });
+    }
+    if (numeros == checked) {
+        simbolos.forEach(function (element, indice, _array) {
+            console.log(element, indice);
+        });
     }
 }
