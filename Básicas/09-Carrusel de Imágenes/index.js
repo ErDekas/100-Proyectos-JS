261c83f565ec821850abc34278c2a6da6aba6a83
ErDekas <pabletor0505@gmail.com>
2024-04-14 00:59:38 +0200

    Creación del HTML, CSS y JS falta incluir las imágenes y hacer las pruebas

---
+++ Básicas/09-Carrusel de Imágenes/index.js
@@ -0,0 +1,32 @@
+let imagenes = [
+    { imagen: "img1.jpg"},
+    { imagen: "img2.jpg"},
+    { imagen: "img3.jpg"},
+    { imagen: "img4.jpg"},
+    { imagen: "img5.jpg"}
+]
+
+const carrusel = document.getElementById('carrusel');
+
+let currentIndex = 0;
+
+function displayCurrentOpinion(){
+    const currentOpinion = imagenes[currentIndex];
+    carrusel.innerHTML = `<img src="${currentOpinion.imagen}" alt="Imagen ${currentIndex + 1}">`;
+}
+
+function nextOpinion(){
+    currentIndex = (currentIndex + 1) % imagenes.length;
+    displayCurrentOpinion();
+}
+
+function prevOpinon(){
+    currentIndex = (currentIndex - 1 + imagenes.length) % imagenes.length;
+    displayCurrentOpinion();
+}
+
+let intervalID;
+
+function startInterval(){
+    intervalID = setInterval(nextOpinion, 3000);
+}
