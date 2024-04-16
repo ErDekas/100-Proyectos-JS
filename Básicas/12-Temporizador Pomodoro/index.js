e6dd9505d77843cc47e52e1f790105583a362305
ErDekas <pabletor0505@gmail.com>
2024-04-16 10:56:56 +0200

    Inicio del JS del pomodoro

---
+++ Básicas/12-Temporizador Pomodoro/index.js
@@ -0,0 +1,7 @@
+const minutos = document.getElementById('pomodoro-timer-min');
+const segundos = document.getElementById('pomodoro-timer-sec');
+const timer = document.getElementById('pomodoro-timer');
+const buttonStart = document.getElementById('pomodoro-button');
+const progress = document.getElementById('pomodoro-progress');
+
+
