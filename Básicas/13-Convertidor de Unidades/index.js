514e9ca3be49c5668a8dcaf778a19fea3336f08e
ErDekas <pabletor0505@gmail.com>
2024-04-18 08:50:26 +0200

    Finalización del convertidor de unidades

--- Básicas/13-Convertidor de Unidades/index.js
+++ Básicas/13-Convertidor de Unidades/index.js
@@ -1,163 +1,157 @@
-// Cantidades a convertir
-const cantidadMasa = document.getElementById('cantidadMasa');
-const cantidadLongitud = document.getElementById('cantidadLongitud');
-const cantidadTemperatura = document.getElementById('cantidadTemperatura');
-// Resultados de la conversión
-const resultadoMasa = document.getElementById('resultadoMasa');
-const resultadoLongitud = document.getElementById('resultadoLongitud');
-const resultadoTemperatura = document.getElementById('resultadoTemperatura');
-// Unidades disponibles a elegir como base
-const masaOriginal = document.getElementById('masaOriginal');
-const longitudOriginal = document.getElementById('longitudOriginal');
-const temperaturaOriginal = document.getElementById('temperaturaOriginal');
-// Unidades disponibles a convertir
-const masaNueva = document.getElementById('masaNueva');
-const longitudNueva = document.getElementById('longitudNueva');
-const temperaturaNueva = document.getElementById('temperaturaNueva');
+function convertirMasa(){
+    let cantidad = parseFloat(document.getElementById('cantidadMasa').value)
+    let masaOriginal = document.getElementById('masaOriginal').value;
+    let masaNueva = document.getElementById('masaNueva').value;
 
-let conversionesMasa = {
-    'kilogramo':{
-        'tonelada': 0.001,
-        'hectogramo': 10,
-        'decagramo': 100,
-        'gramo': 1000,
-        'decigramo': 10000,
-        'centigramo': 100000,
-        'miligramo': 1000000
-    },
-    'tonelada':{
-        'kilogramo': 1000,
-        'hectogramo': 10000,
-        'decagramo': 100000,
-        'gramo': 1000000,
-        'decigramo': 10000000,
-        'centigramo': 100000000,
-        'miligramo': 1000000000
-    },
-    'hectogramo':{
-        'kilogramo': 0.1,
-        'tonelada': 0.0001,
-        'decagramo': 10,
-        'gramo': 100,
-        'decigramo': 1000,
-        'centigramo': 10000,
-        'miligramo': 100000
-    },
-    'decagramo':{
-        'kilogramo': 0.01,
-        'tonelada': 0.00001,
-        'hectogramo': 0.1,
-        'gramo': 10,
-        'decigramo': 100,
-        'centigramo': 1000,
-        'miligramo': 10000
-    },
-    'gramo':{
-        'kilogramo': 0.001,
-        'tonelada': 0.000001,
-        'hectogramo': 0.01,
-        'decagramo': 0.1,
-        'decigramo': 10,
-        'centigramo': 100,
-        'miligramo': 1000
-    },
-    'decigramo':{
-        'kilogramo': 0.0001,
-        'tonelada': 0.0000001,
-        'hectogramo': 0.0001,
-        'decagramo': 0.01,
-        'gramo': 0.1,
-        'centigramo': 10,
-        'miligramo': 100
-    },
-    'centigramo':{
-        'kilogramo': 0.00001,
-        'tonelada': 0.00000001,
-        'hectogramo': 0.00001,
-        'decagramo': 0.0001,
-        'gramo': 0.01,
-        'decigramo': 0.1,
-        'miligramo': 10
-    },
-    'miligramo':{
-        'kilogramo': 0.000001,
-        'tonelada': 0.000000001,
-        'hectogramo': 0.000001,
-        'decagramo': 0.00001,
-        'gramo': 0.0001,
-        'decigramo': 0.0001,
-        'centigramo': 0.01
+    var resultado = 0;
+
+    let gramos = 0;
+    switch (masaOriginal) {
+        case "Toneladas (t)":
+            gramos = cantidad * 1000000; // 1 tonelada = 1000000 gramos
+            break;
+        case "Kilogramo (kg)":
+            gramos = cantidad * 1000; // 1 kilogramo = 1000 gramos
+            break;
+        case "Hectogramo (hg)":
+            gramos = cantidad * 100; // 1 hectogramo = 100 gramos
+            break;
+        case "Decagramo (dag)":
+            gramos = cantidad * 10; // 1 decagramo = 10 gramos
+            break;
+        case "Gramo (g)":
+            gramos = cantidad;
+            break;
+        case "Decigramo (dg)":
+            gramos = cantidad / 10; // 1 decigramo = 0.1 gramos
+            break;
+        case "Centigramo (cg)":
+            gramos = cantidad / 100; // 1 centigramo = 0.01 gramos
+            break;
+        case "Miligramo (mg)":
+            gramos = cantidad / 1000; // 1 miligramo = 0.001 gramos
+            break;
+        default:
+            break;
     }
-};
-let conversionesLongitud = {
-    'kilometro':{
-        'hectometro': 10,
-        'decametro': 100,
-        'metro': 1000,
-        'decimetro':10000,
-        'centimetro': 100000,
-        'milimetro': 1000000
-    },
-    'hectometro':{
-        'kilometro': 0.1,
-        'decametro': 10,
-        'metro': 100,
-        'decimetro':1000,
-        'centimetro': 10000,
-        'milimetro': 100000
-    },
-    'decametro':{
-        'kilometro': 0.01,
-        'hectometro': 0.1,
-        'metro': 10,
-        'decimetro': 100,
-        'centimetro': 1000,
-        'milimetro': 10000
-    },
-    'metro':{
-        'kilometro': 0.001,
-        'hectometro': 0.01,
-        'decametro': 0.1,
-        'decimetro': 10,
-        'centimetro': 100,
-        'milimetro': 1000
-    },
-    'decimetro':{
-        'kilometro': 0.0001,
-        'hectometro': 0.001,
-        'decametro': 0.01,
-        'metro': 0.1,
-        'centimetro': 10,
-        'milimetro': 100
-    },
-    'centimetro':{
-        'kilometro': 0.00001,
-        'hectometro': 0.0001,
-        'decametro': 0.001,
-        'metro': 0.01,
-        'decimetro': 0.1,
-        'milimetro': 10
-    },
-    'milimetro':{
-        'kilometro': 0.000001,
-        'hectometro': 0.00001,
-        'decametro': 0.0001,
-        'metro': 0.001,
-        'decimetro': 0.01,
-        'centimetro': 0.1
+
+    switch (masaNueva) {
+        case "Toneladas (t)":
+            resultado = gramos / 1000000;
+            break;
+        case "Kilogramo (kg)":
+            resultado = gramos / 1000;
+            break;
+        case "Hectogramo (hg)":
+            resultado = gramos / 100;
+            break;
+        case "Decagramo (dag)":
+            resultado = gramos / 10;
+            break;
+        case "Gramo (g)":
+            resultado = gramos;
+            break;
+        case "Decigramo (dg)":
+            resultado = gramos * 10;
+            break;
+        case "Centigramo (cg)":
+            resultado = gramos * 100;
+            break;
+        case "Miligramo (mg)":
+            resultado = gramos * 1000;
+            break;
+        default:
+            break;
     }
-};
-let conversionesTemperatura = {
-    'celsius':{
-        'fahrenheit': 34,
-        'kelvin': 274
-    },
-    'fahrenheit':{
-        'celsius': -17,
-        'kelvin': 256
-    },
-    'kelvin':{
-        'celsius': -272,
-        'fahrenheit': -458
+
+    document.getElementById('resultadoMasa').innerText = resultado;
+}
+function convertirLongitud(){
+    let cantidad = parseFloat(document.getElementById('cantidadLongitud').value);
+    let longitudOriginal = document.getElementById('longitudOriginal').value;
+    let longitudNueva = document.getElementById('longitudNueva').value;
+    var resultado = 0;
+
+    let metros = 0;
+    switch (longitudOriginal) {
+        case "Kilómetro (km)":
+            metros = cantidad * 1000; // 1 kilómetro = 1000 metros
+            break;
+        case "Hectómetro (hm)":
+            metros = cantidad * 100; // 1 hectómetro = 100 metros
+            break;
+        case "Decámetro (dam)":
+            metros = cantidad * 10; // 1 decámetro = 10 metros
+            break;
+        case "Metro (m)":
+            metros = cantidad;
+            break;
+        case "Decimetro (dm)":
+            metros = cantidad / 10; // 1 decímetro = 0.1 metros
+            break;
+        case "Centimetro (cm)":
+            metros = cantidad / 100; // 1 centímetro = 0.01 metros
+            break;
+        case "Milimetro (mm)":
+            metros = cantidad / 1000; // 1 milímetro = 0.001 metros
+            break;
+        default:
+            break;
     }
-};
+    switch (longitudNueva) {
+        case "Kilómetro (km)":
+            resultado = metros / 1000;
+            break;
+        case "Hectómetro (hm)":
+            resultado = metros / 100;
+            break;
+        case "Decámetro (dam)":
+            resultado = metros / 10;
+            break;
+        case "Metro (m)":
+            resultado = metros;
+            break;
+        case "Decimetro (dm)":
+            resultado = metros * 10;
+            break;
+        case "Centimetro (cm)":
+            resultado = metros * 100;
+            break;
+        case "Milimetro (mm)":
+            resultado = metros * 1000;
+            break;
+        default:
+            break;
+    }
+    document.getElementById('resultadoLongitud').innerText = resultado;
+}
+function convertirTemperatura(){
+    let cantidad = parseFloat(document.getElementById('cantidadTemperatura').value);
+    let temperaturaOriginal = document.getElementById('temperaturaOriginal').value;
+    let temperaturaNueva = document.getElementById('temperaturaNueva').value;
+    var resultado = 0;
+
+    if (temperaturaOriginal === temperaturaNueva) {
+        resultado = cantidad; // Si las unidades son las mismas, el resultado es igual a la cantidad original
+    } else if (temperaturaOriginal === "Centígrados (°C)") {
+        if (temperaturaNueva === "Fahrenheit (°F)") {
+            resultado = (cantidad * 9 / 5) + 32; // Fórmula de conversión de Celsius a Fahrenheit
+        } else if (temperaturaNueva === "Kelvin (K)") {
+            resultado = cantidad + 273.15; // Fórmula de conversión de Celsius a Kelvin
+        }
+    } else if (temperaturaOriginal === "Fahrenheit (°F)") {
+        if (temperaturaNueva === "Centígrados (°C)") {
+            resultado = (cantidad - 32) * 5 / 9; // Fórmula de conversión de Fahrenheit a Celsius
+        } else if (temperaturaNueva === "Kelvin (K)") {
+            resultado = (cantidad - 32) * 5 / 9 + 273.15; // Fórmula de conversión de Fahrenheit a Kelvin
+        }
+    } else if (temperaturaOriginal === "Kelvin (K)") {
+        if (temperaturaNueva === "Centígrados (°C)") {
+            resultado = cantidad - 273.15; // Fórmula de conversión de Kelvin a Celsius
+        } else if (temperaturaNueva === "Fahrenheit (°F)") {
+            resultado = (cantidad - 273.15) * 9 / 5 + 32; // Fórmula de conversión de Kelvin a Fahrenheit
+        }
+    }
+
+    document.getElementById('resultadoTemperatura').innerText = resultado;
+}
