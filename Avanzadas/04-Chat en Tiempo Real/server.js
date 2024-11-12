416135346e3165d92764e23994fdf72d73aafb09
ErDekas <pabletor0505@gmail.com>
2024-11-12 09:25:10 +0100

    Arreglo final

--- Avanzadas/04-Chat en Tiempo Real/server.js
+++ Avanzadas/04-Chat en Tiempo Real/server.js
@@ -1,10 +1,10 @@
 // server.js
 const express = require("express");
-const https = require("https");
+const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-const server = https.createServer(app);
+const server = http.createServer(app);
 const io = new Server(server);
 const PORT = 3000;
 
