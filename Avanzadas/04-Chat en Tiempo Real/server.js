f5d33bbf78baea4ae29c02dc4cdb183fa8a0ff91
ErDekas <pabletor0505@gmail.com>
2024-11-12 10:09:26 +0100

    Arreglo para el despliegue

--- Avanzadas/04-Chat en Tiempo Real/server.js
+++ Avanzadas/04-Chat en Tiempo Real/server.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const PORT = 3000;
+app.listen(process.env.PORT || 3000);
 
 // Servir archivos estáticos
 app.use(express.static("public"));
