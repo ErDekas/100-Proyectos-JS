6a93f7aa6ac3bb3eaeb08b33062c23cb8578f957
ErDekas <pabletor0505@gmail.com>
2024-11-13 22:23:05 +0100

    Arreglo config vercel.json

--- Avanzadas/04-Chat en Tiempo Real/server.js
+++ Avanzadas/04-Chat en Tiempo Real/server.js
@@ -5,7 +5,12 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 const PORT = process.env.PORT || 3000;
 
 // Servir archivos estáticos
